e2cf76e112d6b551dc559e38110a3c1a73b2ef61
Jeet <83248565+TangibleTensor@users.noreply.github.com>
2025-10-03 13:15:08 +0100

    Added some endpoints for the User table. Haven't changed it so that it doesnt expose everything to the front end yet

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,5 +1,20 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-
-class CreateUserRequest(BaseModel):
+class _UsernameRequest(BaseModel):
     username: str
+
+class CreateUserRequest(_UsernameRequest):
+    pass
+    
+class DeleteUserRequest(_UsernameRequest):
+    pass
+
+class GetUserRequest(_UsernameRequest):
+    pass
+
+class UpdateUserLastLoginRequest(_UsernameRequest):
+    last_login: datetime
+    
+class UpdateUserActivityRequest(_UsernameRequest):
+    is_active: bool
--- backend/services/users.py
+++ backend/services/users.py
@@ -1,13 +1,61 @@
 from sqlalchemy.orm import Session
-
 from backend.database import User
+from sqlalchemy.exc import IntegrityError
+from schemas import UserSchema
+from datetime import datetime
 
 
-def create_user(db: Session, username: str) -> User:
-    """Create a new user account."""
+def create_user(db: Session, username: str) -> User | None:
+    """Create and return new user account. Returns None if username exists."""
     user = User(username=username)
-    # TODO: Error handling (e.g., user already exists).
-    # validate_new_user(db, user)
     db.add(user)
+    try:
+        db.flush()
+        return user
+    except IntegrityError:
+        db.rollback()
+        return None
+
+def get_user(db: Session, username: str) -> User | None:
+    """Delete a user account. Returns the user if username exists, else None"""
+    user = db.query(User).filter(User.username == username).first()
+    
+    return user
+
+# region User update services
+
+def update_user_activity(db: Session, username: str, is_active: bool) -> datetime | None:
+    """Update a user account. Returns None if username doesn't exist"""
+    user = db.query(User).filter(User.username == username).first()
+    
+    if user is None:
+        return None
+    
+    user.is_active = is_active
+    
+    
+def update_user_last_login(db: Session, username: str, last_login: datetime) -> datetime | None:
+    """Update a user account. Returns None is username doesn't exist"""
+    user = db.query(User).filter(User.username == username).first()
+    
+    if user is None:
+        return None
+    
+    user.last_login = last_login
+
+# endregion
+
+
+def delete_user(db: Session, username: str) -> User | None:
+    """Delete a user account. Returns the user if username exists, else None"""
+    user = db.query(User).filter(User.username == username).first()
+    
+    if user is None:
+        return None
+    
+    db.delete(user)
     db.flush()
     return user
+    
+
+
